5321eeae67c1171d9e616314cbf0a8c27e49292d
D3S0X <nicolas.haid@gmail.com>
2018-11-05 22:36:32 +0100

    Update uncurator.js

--- uncurator.js
+++ uncurator.js
@@ -9,7 +9,8 @@ var clickEvent = new MouseEvent("click", {
 
 var c = 0;
 
-#maybe not the best way but it does it's job and works.
+#maybe not the best way but it does it's job and works
+
 var timer = setInterval(function() {
 	window.scrollTo(0, document.body.scrollHeight);
 	var done = (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight;
